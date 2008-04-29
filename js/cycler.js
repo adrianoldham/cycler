7f292510e45a3260d4b2ceaa0e26f7cad0e4c4a7
Adrian Oldham <adriann@visualthought.com.au>
2008-04-29 13:54:03 +0000

    updated "cyclerStatus" to include "innerHTML" and styled the positioning n the demo, also tidied up general code formatting

--- js/cycler.js
+++ js/cycler.js
@@ -20,24 +20,24 @@ Cycler.prototype = {
 			originalArticleElements.remove(rand);
 			this.articleElements.push(articleElement);
 		}
-	  
+
 	  if (this.articleElements.length < 2) return;
-	  
+
 	  this.scroller     = null;
 	  this.currentChild = this.articleElements[0];
 	  this.delay        = delay || DEFAULT_DELAY;
-	  
+
 	  // parent of the article elements is the container/cycle region
 	  this.cycleRegion  = this.currentChild.parentNode;
-	  
+
 	  var wrapper = new Element("div");
 	  wrapper.style.position = "relative";
 	  wrapper.addClassName("cycle_wrapper");
-	  
+
 	  this.cycleRegion.insertBefore(wrapper, this.currentChild);
-	  
+
 	  var wrappedElements = new Array();
-	  
+
 	  // only show first article at start up
 	  first = true;
 	  this.articleElements.each(function(e) {
@@ -46,66 +46,68 @@ Cycler.prototype = {
 	    wrappedArticle.addClassName("cycle_element");
 	    
 	    wrappedArticle.appendChild(e);
-	    wrapper.appendChild(wrappedArticle);	    
+	    wrapper.appendChild(wrappedArticle);
 	    wrappedElements.push(wrappedArticle);
-	    
+
 	    if (first == false) wrappedArticle.hide();
-      first = false;
+	  first = false;
 	  });
-	  
+
 	  this.currentIndex = 0;
 	  this.currentChild = wrappedElements[0];
-	  
-    if (statusDivID != null) {
-      // cycle status div is specified
-      this.cycleStatus = $(statusDivID);
-    }
-    else {
-      // if no cycle status div, then we create one with class "cycle_play"
-      this.cycleStatus = new Element("div");
 
-      this.cycleStatus.addClassName("cycle_play");
-      wrapper.insertBefore(this.cycleStatus, this.currentChild);
-    }
-    
-    //this.cycleStatus.hide();
-	  
-    // on mouse over, stop cycling
-		Event.observe(wrapper, "mouseover", this.stopCycle.bind(this));
-		
-		// on mouse out, start cycling again
-		Event.observe(wrapper, "mouseout", this.startCycle.bind(this));
-		
+	if (statusDivID != null) {
+	  // cycle status div is specified
+	  this.cycleStatus = $(statusDivID);
+	}
+	else {
+	  // if no cycle status div, then we create one with class "cycle_play"
+	  this.cycleStatus = new Element("div");
+
+	  this.cycleStatus.addClassName("cycle_play");
+	  wrapper.insertBefore(this.cycleStatus, this.currentChild);
+	}
+
+	  //this.cycleStatus.hide();
+
+	  // on mouse over, stop cycling
+	  Event.observe(wrapper, "mouseover", this.stopCycle.bind(this));
+
+	  // on mouse out, start cycling again
+	  Event.observe(wrapper, "mouseout", this.startCycle.bind(this));
+
 	  this.cycleRegion = wrapper;
 	  this.articleElements = wrappedElements;
 	},
-	
+
 	startCycle: function() {
 	  // switch off is cycling
 	  //this.cycleStatus.hide();
-      this.cycleStatus.addClassName("cycle_play");
-      this.cycleStatus.removeClassName("cycle_pause");
+	  this.cycleStatus.addClassName("cycle_play");
+	  this.cycleStatus.removeClassName("cycle_pause");
+	  this.cycleStatus.innerHTML = "Playing";
 	  
 	  if (this.scroller != null) this.scroller.stop();
 	  this.scroller = new PeriodicalExecuter(this.switchIt.bind(this), this.delay);
 	},
-	
+
 	stopCycle: function() {
 	  // switch on if paused
 	  //this.cycleStatus.show();
-      this.cycleStatus.addClassName("cycle_pause");
-	  
+	  this.cycleStatus.addClassName("cycle_pause");
+	  this.cycleStatus.innerHTML = "Paused";
+
 	  if (this.scroller != null) this.scroller.stop();
 	},
-	
+
 	switchIt: function() {
 	  this.oldChild = this.currentChild;
 	  
 	  // find the next sibling article with the same class name
 	  this.currentIndex++;
-    this.currentIndex %=  this.articleElements.length;
-    
-    this.currentChild = this.articleElements[this.currentIndex];
+	  this.currentIndex %=  this.articleElements.length;
+	  
+	  this.currentChild = this.articleElements[this.currentIndex];
 	  
 	  // loop back to start if we reach the end
 	  if (this.currentChild == null) this.currentChild = this.articleElements[0];
@@ -114,19 +116,19 @@ Cycler.prototype = {
 	},
 	
 	doTransition: function() {
-    // do transitions
+	  // do transitions
 	  this.currentChild.style.position = "absolute";
-    this.currentChild.style.top = "0";
-    this.currentChild.style.left = "0";
-    this.currentChild.style.width = "100%";
+	  this.currentChild.style.top = "0";
+	  this.currentChild.style.left = "0";
+	  this.currentChild.style.width = "100%";
 
-    this.currentChild.style.zIndex = 100;
+	  this.currentChild.style.zIndex = 100;
 
 	  this.currentChild.visualEffect('appear', { beforeFinish: this.revertArticle.bind(this) });
 	},
 	
 	revertArticle: function() {
-    this.currentChild.style.position = "";
-    this.oldChild.hide();
+	  this.currentChild.style.position = "";
+	  this.oldChild.hide();
 	}
 }
